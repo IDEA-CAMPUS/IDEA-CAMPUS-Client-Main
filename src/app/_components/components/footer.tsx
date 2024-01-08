8cbfe23ca247017eef43797f67fa89d6e291f43b
VsangkyeongV <137258368+VsangkyeongV@users.noreply.github.com>
2024-01-08 16:47:54 +0900

    FEAT:: footer 컴포넌트

---
+++ src/app/_components/components/footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <div className="w-full h-[196px] box-border pl-[85px] pt-[65px]">
+      <div>
+        {`Copyright © IdeaCampus. All Rights Reserved 
+        E-mail : wotjr0487@kakao.com`}
+      </div>
+    </div>
+  );
+};
